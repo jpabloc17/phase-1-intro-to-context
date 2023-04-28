86c973ae97020feba81582abf81cc14c3cc17d7a
Pablo Castro <pablocas-tro1@outlook.com>
2023-04-28 11:24:13 -0500

    Completed Lab

--- index.js
+++ index.js
@@ -1,1 +1,76 @@
 // Your code here
+// const array = ["Juan", "Castro", "SD", 21.40,]
+function createEmployeeRecord(array){
+    return {
+        firstName: array[0],
+        familyName: array[1],
+        title: array[2],
+        payPerHour: array[3],
+        timeInEvents: [],
+        timeOutEvents: []
+    }
+}
+
+function createEmployeeRecords(employeeRecords){
+    return employeeRecords.map(function(element){
+        return createEmployeeRecord(element);
+    })
+}
+
+function createTimeInEvent(recordObj, dateStamp){
+    const [date, hour] = dateStamp.split(" ");
+    recordObj.timeInEvents.push({
+        type: "TimeIn",
+        hour: parseInt(hour),
+        date: date,
+    });
+    return recordObj;
+}
+
+function createTimeOutEvent(recordObj, dateStamp){
+    const [date, hour] = dateStamp.split(" ");
+    recordObj.timeOutEvents.push({
+        type: "TimeOut",
+        hour: parseInt(hour),
+        date: date,
+    })
+    return recordObj
+}
+
+function hoursWorkedOnDate(recordObj, dateStamp){
+    const timeInEvent = recordObj.timeInEvents.find(
+        function(timeIn){
+            return timeIn.date === dateStamp
+        }
+    );
+    const timeOutEvent = recordObj.timeOutEvents.find(
+        function(timeOut){
+            return timeOut.date === dateStamp
+        }
+    );
+    return (timeOutEvent.hour - timeInEvent.hour) / 100;
+}
+
+function wagesEarnedOnDate(recordObj, date){
+    const hoursWorked = hoursWorkedOnDate(recordObj, date);
+    return hoursWorked * recordObj.payPerHour;
+}
+
+function allWagesFor(recordObj) {
+    let totalWages = 0;
+    recordObj.timeInEvents.map((timeInEvent) => {
+      const date = timeInEvent.date;
+      totalWages += wagesEarnedOnDate(recordObj, date);
+    });
+    return totalWages;
+  };
+
+function calculatePayroll(arrayOfEmployee){
+    const totalPayroll = arrayOfEmployee.reduce((total, employeeRecord) => {
+        return total + allWagesFor(employeeRecord);
+    }, 0);
+        return totalPayroll;
+  }
+
+
+// console.log(createEmployeeRecord(["Juan", "Castro", "SD", 21.40,]))
